37b6ff505435232b0fe2b5604aa9bcaaa3983fe2
Christian Valente <christian.valente@outlook.com>
2020-10-13 17:28:11 +0300

    Minor modifications to the build system

--- csharp/cplex/api/Properties/AssemblyInfo.cs
+++ csharp/cplex/api/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AMPLAPI")]
-[assembly: AssemblyDescription("AMPL Api for .NET")]
+[assembly: AssemblyTitle("ampls")]
+[assembly: AssemblyDescription("ampls: Simply solver libraries")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AMPL Inc")]
-[assembly: AssemblyProduct("AMPLAPI")]
-[assembly: AssemblyCopyright("Copyright 2017")]
+[assembly: AssemblyProduct("ampls")]
+[assembly: AssemblyCopyright("Copyright 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2d7cbd88-15b4-46df-9ffb-b5d5ef6a120d")]
+[assembly: Guid("2d7cbd88-15b4-46df-9ffb-b5d5bb6a120d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("")]
-[assembly: AssemblyFileVersion("")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
