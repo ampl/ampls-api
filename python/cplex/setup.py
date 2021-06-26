857ed9ed6bdf0a4307b092480c949ad50b5ae475
Filipe Brandão <fdabrandao@gmail.com>
2021-06-26 16:16:18 +0100

    Bump version number to release python packages

--- python/cplex/setup.py
+++ python/cplex/setup.py
@@ -72,7 +72,7 @@ def link_args():
 
 setup(
     name='amplpy_cplex',
-    version='0.1.0b11',
+    version='0.1.0b12',
     description='CPLEX extension for amplpy',
     long_description=__doc__,
     license='BSD-3',
