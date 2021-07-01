5c03b39e570de02756d6a48f3535cae5d921c123
Christian Valente <christian.valente@outlook.com>
2021-07-01 20:16:33 +0300

    Amended model to use tsplib95 to load tsp data files.

--- python/examples/tsp_simple_cuts.py
+++ python/examples/tsp_simple_cuts.py
@@ -1,14 +1,15 @@
-from amplpy import AMPL
-import amplpy_cplex as ampls
+from amplpy import AMPL, DataFrame
+import amplpy_gurobi as ampls
 from patch import  *
-
+import time
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
+import tsplib95 as tsp
 
 tspAMPLModel = """set NODES ordered;
-param hpos {NODES} >= 0;
-param vpos {NODES} >= 0;
+param hpos {NODES};
+param vpos {NODES};
 
 set PAIRS := {i in NODES, j in NODES: ord(i) < ord(j)};
 
@@ -22,39 +23,67 @@ minimize Tour_Length: sum {(i,j) in PAIRS} distance[i,j] * X[i,j];
 subject to Visit_All {i in NODES}:
    sum {(i,j) in PAIRS} X[i,j] + sum {(j,i) in PAIRS} X[j,i] = 2;"""
 
-tspAMPLData = """data; 
-param: NODES: hpos vpos :=
-    N01  0 0
-    N02  0 1
-    N03  0 2
-    N04  0 4
-    N05  1 2
-    N06  1 3
-    N07  2 0
-    N08  2 4
-    N09  3 4
-    N10  4 4
-    N11  5 0
-    N12  6 1
-    N13  7 2
-    N14  7 4
-    N15  4 2
-    N16  8 3
-    N17  8 4 ;"""
-
+PLOTSUBTOURS = True
 solver = "gurobi"
 ampl = AMPL()
 ampl.eval(tspAMPLModel)
-ampl.eval(tspAMPLData)
 ampl.option["solver"]=solver
+
+
+def getDataFrameFromTspFile(tspFile):
+  p = tsp.load(tspFile)
+  if not p.is_depictable:
+    printf("Problem is not depictable!")
+
+  # Amendments as we need the nodes lexographically ordered
+  nnodes = len(list(p.get_nodes()))
+  i = 0
+  while nnodes>1:
+    nnodes = nnodes/10
+    i+=1
+  formatString = f"{{:0{i}d}}"
+  nodes = {formatString.format(value) : p.node_coords[index+1] for index, value in enumerate(p.get_nodes())}
+  df = DataFrame(index=[('NODES')],
+            columns=['hpos', 'vpos'])
+  df.setValues(nodes)
+  return df
+
+# Set problem data from tsp file
+df = getDataFrameFromTspFile("e:/downloads/airports.tsp")
+ampl.setData(df, "NODES")
 
 # Set some globals that never change during the execution of the problem
 NODES = set(ampl.getData("NODES").toList())
 POINTS = ampl.getData("hpos,vpos").toList()
+
 CPOINTS = {node : complex(x, y) for (node, x, y) in POINTS}
 
 
+def plotTours(tours: list, points_coordinate: dict):
+    # Plot all the tours in the list each with a different color
+    colors = ['b', 'g', 'c', 'm', 'y', 'k']
+    for i, tour in enumerate(tours):
+        tourCoordinates = [points_coordinate[point.strip("'")] for point in tour]
+        color = colors[i % len(colors)]
+        plot_all(tourCoordinates, color = color)
+    plt.show()
 
+def plot_all(tour, alpha=1, color=None):
+    # Plot the tour as blue lines between blue circles
+    plotline(list(tour) + [tour[0]], alpha=alpha, color=color)
+    plotline([tour[0]], 's', alpha=alpha, color=color)
+    
+def plotline(points, style='o-', alpha=1, color=None):
+    "Plot a list of points (complex numbers) in the 2-D plane."
+    X, Y = XY(points)
+    if color:
+        plt.plot(X, Y, style, alpha=alpha, color=color)
+    else:
+        plt.plot(X, Y, style, alpha=alpha)
+    
+def XY(points):
+    "Given a list of points, return two lists: X coordinates, and Y coordinates."
+    return [p.real for p in points], [p.imag for p in points]
 
 
 # Graphs helper routines
@@ -76,11 +105,11 @@ def trasverse(node, arcs : set, allnodes : set, subtour = None) -> list:
    destination = [(i) for i in myarc if i != node][0]
    # Remove from arcs and nodes to visit
    arcs.remove(myarc)
-   allnodes.remove(node)
+   if node in allnodes:
+      allnodes.remove(node)
 
    trasverse(destination, arcs, allnodes, subtour)
    return subtour
-
 
 def findSubTours(arcs : set, allnodes : set):
    """Find all the subtours defined by a set of arcs and
@@ -92,8 +121,8 @@ def findSubTours(arcs : set, allnodes : set):
      subtours.append(l)
    if len(subtours) > 1:
      print(f"Found {len(subtours)} subtours:")
-     for st in subtours:
-        print(st)
+    # for st in subtours:
+    #    print(st)
    return subtours
 
 
@@ -124,7 +153,8 @@ def amplSubTourElimination(ampl : AMPL):
     # If we have only one tour, the solution is valid
     if len(subtours) <= 1:
       break
-    plotTours(subtours, CPOINTS)
+    if PLOTSUBTOURS:
+      plotTours(subtours, CPOINTS)
     # Else add the current tours to the list of subtours
     allsubtours.extend(subtours)
     # And add those to the constraints by assigning the values to
@@ -165,31 +195,42 @@ def solverSubTourElimination(ampl : AMPL, solver):
 
 # Callback class that actually add the cuts if subtours are found in a solution
 class MyCallback(ampls.GenericCallback):
+   def __init__(self):
+     super().__init__()
+     self.iteration = 0
    def run(self):
      try:
         # For each solution
         if self.getAMPLWhere() == ampls.Where.MIPSOL:
+          self.iteration += 1
+          print(f"\nIteration {self.iteration}: Finding subtours")
           sol = self.getSolutionVector()
           arcs = [xvars[i] for i,value in enumerate(sol) if value > 0]
           subTours = findSubTours(set(arcs), set(vertices))
           if len(subTours) ==1:
-            print("No subtours detected. Ending optimization.")
-            return -1
-          plotTours(subTours, CPOINTS)
-          for subTour in subTours[:-1]:
+            print("No subtours detected.")
+            return 0
+          print(f"Adding {len(subTours)} cuts")
+          if PLOTSUBTOURS:
+            plotTours(subTours, CPOINTS)
+          for subTour in subTours:
             st1 = set(subTour)
             nst1 = set(vertices) - st1
             externalArcs = [(i,j) if i < j else (j,i) for i in st1 for j in nst1]
             varsExternalArcs = [xinverse[(i,j)] for (i,j) in externalArcs]
             coeffs = [1 for i in range(len(varsExternalArcs))]
             varsExternalArcs = sorted(varsExternalArcs)
-            print(f"Adding cut {varsExternalArcs}")
+            if False:
+              print(f"Adding cut {varsExternalArcs}")
             self.addLazyIndices(varsExternalArcs , coeffs,
                                   ampls.CutDirection.GE, 2)
+            if len(subTours) == 2:
+              return 0
+          print("Continue solving")
         return 0
      except Exception as e:
        print('Error:', e)
-       return 1
+       return 0
 
 # Set to true to use AMPL, False to use solver callbacks
 doAMPL = False
