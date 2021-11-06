782ad5e526694def3e6dfc14926bd9889fa29ca4
Filipe Brandão <fdabrandao@gmail.com>
2021-11-06 18:01:36 +0000

    Add favicon and specify theme version

--- doc/rst/conf.py
+++ doc/rst/conf.py
@@ -68,6 +68,7 @@ html_context = {
 html_css_files = [
     'css/custom.css',
 ]
+html_favicon = "_static/cropped-favicon-raw-192x192.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
