03c70989d9516cdc19b6df08b7a63e3066a22490
Christian Valente <christian.valente@outlook.com>
2023-05-15 18:41:45 +0300

    Fixing names

--- python/cplexmp/swig.sh
+++ python/cplexmp/swig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd `dirname $0`
-cd amplpy_cplex/swig
+cd amplpy_cplexmp/swig
 swig -python -c++ -builtin \
     -I../cpp/ampls/include \
-    -I../cpp/cplex/include \
+    -I../cpp/cplexmp/include \
     -I../libs/cplex/include \
-    amplpy_cplex_swig.i
+    amplpy_cplexmp_swig.i
