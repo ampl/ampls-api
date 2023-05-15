cea3607b4c017571b15463a5c44a726be7a0c8cc
Christian Valente <christian.valente@outlook.com>
2023-05-15 18:37:48 +0300

    Updated again for simultaneous changes

--- libs/download.sh
+++ libs/download.sh
@@ -2,4 +2,4 @@
 cd "`dirname "$0"`"
 curl -OLk https://ampl.com/dl/fdabrandao/solver-public-libs.zip
 unzip solver-public-libs.zip
-#cp -r solver-public-libs/solver-public-libs/* .
+cp -r solver-public-libs/* .
