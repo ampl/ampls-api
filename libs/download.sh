d02551b46ec24f576c6a567cbb3afded565aeaa3
Filipe Brandão <fdabrandao@gmail.com>
2023-05-16 20:02:39 +0100

    Adjust download.sh to new zip structure

--- libs/download.sh
+++ libs/download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "`dirname "$0"`"
+set -ex
 curl -OLk https://ampl.com/dl/fdabrandao/solver-public-libs.zip
 unzip solver-public-libs.zip
-cp -r solver-public-libs/* .
+cp -r solver-public-libs/solver-public-libs/* .
