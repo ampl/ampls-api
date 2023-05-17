4a77895c5121e9952992ce8157ebd4ed54287fcd
Filipe Brandão <fdabrandao@gmail.com>
2023-05-17 13:08:56 +0100

    Update solver-public-libs.zip URL

--- libs/download.sh
+++ libs/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "`dirname "$0"`"
 set -ex
-curl -OLk https://ampl.com/dl/fdabrandao/patch/solver-public-libs.zip
+curl -OLk https://ampl.com/dl/fdabrandao/solver-public-libs.zip
 unzip solver-public-libs.zip
 cp -r solver-public-libs/solver-public-libs/* .
