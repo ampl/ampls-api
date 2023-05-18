443da41bb7d8fd11f5fe0285094d25092cf69c44
Filipe Brandão <fdabrandao@gmail.com>
2023-05-18 14:29:43 +0100

    Pass environement variable via cibuildwheel

--- python/gurobi/amplpy_gurobi/tests/__main__.py
+++ python/gurobi/amplpy_gurobi/tests/__main__.py
@@ -3,8 +3,15 @@
 from __future__ import absolute_import
 
 import unittest
+import os
 
 from .test_callbacks import TestCallbacks
 
 if __name__ == "__main__":
+    from amplpy import modules
+
+    uuid = os.environ.get("AMPLKEY_UUID", None)
+    if uuid is not None and uuid != "":
+        modules.activate(uuid)
+
     unittest.main()
