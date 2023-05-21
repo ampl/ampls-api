d2c8aef2e63ee5d3ca608eeebcc0261c22a4d970
Christian Valente <christian.valente@outlook.com>
2023-05-21 11:43:15 +0100

    Added walk-through example

---
+++ python/examples/multiple_models.py
@@ -0,0 +1,118 @@
+"""A script that shows how to create multiple models with AMPL, export an AMPLs instance
+for a specific solver, set some solver options and get status/solution, and get the results
+back in AMPL"""
+
+import amplpy
+import amplpy_gurobi
+import pandas as pd
+from typing import Any, Dict
+
+def makeAmplModel(numVars:int=10, flipObjective:bool = False, makeInfeasible:bool = False, 
+                  presolveLevel : bool = 10):
+    """ Create an instance of AMPL and a model"""
+    ampl = amplpy.AMPL()
+    # Note that if a model is detected as infeasible by the AMPL presolver,
+    # it will not be exported to the solver, and an exception will be thrown.
+
+    ampl.set_option("presolve", presolveLevel)
+    
+    ampl.eval("""
+    set varIndexes within Integers;
+    set constraintIndexes within Integers;
+
+    param varCosts { varIndexes };
+    param rightHandSides { constraintIndexes };
+
+    var x {i in varIndexes} >=0;
+
+    maximize objectiveFunction:
+        sum { i in varIndexes } varCosts[i] * x[i];
+
+    subject to mainConstraints { j in constraintIndexes } :
+        x[j] + x[j+1] <= rightHandSides[j];"""
+    )
+
+    varIndexDf = pd.DataFrame(
+        data = [(i,i if not flipObjective else -i) for i in range(numVars)],
+        columns = ["varIndexes","varCosts"]
+    ).set_index("varIndexes")
+    ampl.setData(varIndexDf,set_name = "varIndexes")
+    
+    constraintIndexesDf = pd.DataFrame(
+        data = [(i,1 if not makeInfeasible else -1) for i in range(numVars-1)],
+        columns = ["constraintIndexes","rightHandSides"]
+    ).set_index("constraintIndexes")
+    ampl.setData(constraintIndexesDf,set_name = "constraintIndexes")
+    return ampl
+
+def solveModel(ampl:amplpy.AMPL, options:Dict[str,Any] = {}, useNativeCall: bool=True):
+    model= ampl.export_gurobi_model()
+    for o, v in options.items():
+        model.set_option(o,v) # Set options from
+       # ampl driver (see https://dev.ampl.com/solvers/gurobi/options.html)
+       # Most of them will work the same way across solvers
+    
+    # For specific solvers (e.g. gurobi) there are also wrappers for the 
+    # gurobi parameter accessors. See https://www.gurobi.com/documentation/10.0/refman/c_parameter_examples.html
+    model.set_param("Threads", 4) 
+
+    # A third option is to use the gurobi C API directy, by getting access to the underlying 
+    # gurobi model/environment pointer. This is suitabe to use functionalities provided
+    # by the solver C API
+    # The two statmeents below are therefore equivalent, the first uses the (generic) function
+    # call, the latter uses the Gurobi C API directly:
+    if useNativeCall:
+        amplpy_gurobi.GRBoptimize(model.get_grb_model())
+    else:
+        model.optimize()
+    ampl.import_solution(model)
+    return model
+
+
+def createAndSolveSimpleModel():
+    ampl = makeAmplModel()
+    model = solveModel(ampl)
+    
+    print(f"Status is {model.get_status().name}")
+    assert model.get_status() == amplpy_gurobi.Status.OPTIMAL
+
+    # Num Vars is even -> last var idx is odd -> expect odd idx vars to be 1
+    # Num Vars is odd -> last var idx is even -> expect even idx vars to be 1
+    expectedSolution = [abs(i%2-numVars%2) for i in range(numVars)]    
+    expectedObjective = sum( [i*expectedSolution[i] for i in range(numVars)])    
+    solverObjective = model.getObj()
+    assert solverObjective == expectedObjective
+    # Check that AMPL has received the correct objective
+    assert solverObjective == ampl.getCurrentObjective().value()
+    print("Completed Simple Model Test.")
+
+def createAndSolveInfeasibleModel(presolveLevel : bool = 10):
+    ampl = makeAmplModel(makeInfeasible=True, presolveLevel=presolveLevel)
+    # Set some solver options
+    # Setting iisfind to 1 to find the Irreducible Infeasibility Subset
+    model = solveModel(ampl, {"pre:scale": 3, "outlev" : 1, "iisfind" : 1})
+    print(f"Status is {model.get_status().name}")
+    assert model.get_status() == amplpy_gurobi.Status.INFEASIBLE
+    
+    # Display IIS for infeasible model by converting it to a pandas dataframe
+    print(ampl.get_data("_varname, _var.iis").to_pandas().set_index('_varname'))
+    print(ampl.get_data("_conname, _con.iis").to_pandas().set_index('_conname'))
+    print("Completed Infeasible Model Test.")
+
+
+if __name__ == "__main__":
+    
+    try:
+        # With presolve set to a high level, AMPL detects the infeasibilitty
+        # and does not export the model. Throws an explainatory runtime error
+        createAndSolveInfeasibleModel(10)
+    except RuntimeError as e:
+        print(e)
+    # Turning off presolve makes ampl actually export the model
+    # In the fucntion we'll set some options to find the source of the infeasibility
+    createAndSolveInfeasibleModel(0)
+
+    # Repeated solves below for feasible model:
+    createAndSolveSimpleModel()
+    createAndSolveSimpleModel()
+    
