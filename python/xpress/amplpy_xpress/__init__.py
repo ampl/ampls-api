e4416115a285d21264e08be2b5e936933df01ca3
Filipe Brandão <fdabrandao@gmail.com>
2023-05-25 11:28:42 +0100

    Test xpress with amplpy import before updating PATH

--- python/xpress/amplpy_xpress/__init__.py
+++ python/xpress/amplpy_xpress/__init__.py
@@ -1,31 +1,38 @@
 import sys
 import os
 import platform
+
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-sys.path.append(os.path.join(BASEDIR, 'swig'))
+sys.path.append(os.path.join(BASEDIR, "swig"))
 
-if platform.system() == 'Windows':
-    sysdir = 'win64'
-elif platform.system() == 'Linux':
-    sysdir = 'linux64'
-elif platform.system() == 'Darwin':
-   sysdir = 'osx64'
+if platform.system() == "Windows":
+    sysdir = "win64"
+elif platform.system() == "Linux":
+    sysdir = "linux64"
+elif platform.system() == "Darwin":
+    sysdir = "osx64"
 else:
     print("Platform not recognized: {}", platform.system())
 
-LIBPATH=os.path.join(BASEDIR, 'libs', 'xpress', 'lib', sysdir)
+LIBPATH = os.path.join(BASEDIR, "libs", "xpress", "lib", sysdir)
+import amplpy
+
 # Add path to help finding the license file
 os.environ["PATH"] = LIBPATH + os.pathsep + os.environ["PATH"]
 
-if platform.system() == 'Windows':
+if platform.system() == "Windows":
     import ctypes
-    for dll in ['xprl.dll', 'xprs.dll','xpress-lib.dll']:
+
+    for dll in ["xprl.dll", "xprs.dll", "xpress-lib.dll"]:
         try:
             ctypes.CDLL(os.path.join(LIBPATH, dll))
         except Exception as e:
-            print("Problem importing library {}:\n{}\n".format(os.path.join(LIBPATH, dll), e))
-
+            print(
+                "Problem importing library {}:\n{}\n".format(
+                    os.path.join(LIBPATH, dll), e
+                )
+            )
 
 
 try:
@@ -33,4 +40,4 @@ try:
 except:
     raise
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
