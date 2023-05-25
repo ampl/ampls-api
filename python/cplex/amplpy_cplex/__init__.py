7d69beee8f979b687190ffca7ddb0a89cff0e486
Christian Valente <christian.valente@outlook.com>
2023-05-25 18:46:11 +0200

    Update paths for cplex/windows

--- python/cplex/amplpy_cplex/__init__.py
+++ python/cplex/amplpy_cplex/__init__.py
@@ -4,27 +4,29 @@ import platform
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-sys.path.append(os.path.join(BASEDIR, "swig"))
+sys.path.append(os.path.join(BASEDIR, 'swig'))
 
-if platform.system() == "Windows":
-    sysdir = "win64"
-elif platform.system() == "Linux":
-    sysdir = "linux64"
-elif platform.system() == "Darwin":
-    sysdir = "osx64"
+if platform.system() == 'Windows':
+    sysdir = 'win64'
+elif platform.system() == 'Linux':
+    sysdir = 'linux64'
+elif platform.system() == 'Darwin':
+    sysdir = 'osx64'
 else:
-    print(f"Platform not recognized: {platform.system()}")
+    print(f'Platform not recognized: {platform.system()}')
 
-if platform.system() == "Windows":
+LIBPATH = os.path.join(BASEDIR, "libs", "xpress", "lib", sysdir)
+os.environ["PATH"] = LIBPATH + os.pathsep + os.environ["PATH"]
+
+if platform.system() == 'Windows':
     import ctypes
-
-    LIBPATH = os.path.join(BASEDIR, "libs", "cplex", "lib", "win64")
-    for dll in ["cplex2211.dll", "cplexmp-lib.dll"]:
+    
+    for dll in ['cplex2211.dll', 'cplexmp-lib.dll']:
         try:
             ctypes.CDLL(os.path.join(LIBPATH, dll))
         except Exception as e:
             print(
-                "Problem importing library {}:\n{}\n".format(
+                'Problem importing library {}:\n{}\n'.format(
                     os.path.join(LIBPATH, dll), e
                 )
             )
@@ -35,4 +37,4 @@ try:
 except:
     raise
 
-__version__ = "0.1.0"
+__version__ = '0.1.0'
