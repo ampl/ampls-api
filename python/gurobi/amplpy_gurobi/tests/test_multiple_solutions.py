5e9b115a5ea081c83b6ef1805bb945ee82986d06
Filipe Brandão <fdabrandao@gmail.com>
2023-05-25 16:41:41 +0100

    Use snake_case names and add more assertions to the tests

--- python/gurobi/amplpy_gurobi/tests/test_multiple_solutions.py
+++ python/gurobi/amplpy_gurobi/tests/test_multiple_solutions.py
@@ -13,38 +13,33 @@ import amplpy_gurobi as ampls
 SOLVER = "gurobi"
 
 
-def create_model() -> AMPL:
-    """Create an instance of AMPL and a model"""
-    ampl = AMPL()
-    ampl.eval(
-        """var x binary; var y binary; var z binary;
-    minimize TotalSum: z + 1;
-    subj to C1 : x + y >= 1;"""
-    )
-    return ampl
-
-
-def solve_model(ampl: AMPL):
-    model = ampl.to_ampls(SOLVER)
-    model.setOption("sol:stub", "stub")
-    try:
-        model.setOption("sol:poolgap", 0.1)
-    except:
-        pass
-    model.refresh()
-    model.optimize()
-    ampl.import_solution(model)
-    v = int(ampl.get_value("TotalSum.nsol"))
-    if SOLVER != "xpress":  # not supported right now
-        assert v == 3
-    print(f"Got {v} solutions")
-    for i in range(1, v + 1):
-        ampl.import_solution("stub", i)
-
-
 class TestMultipleSolutions(TestBase):
     def test_multiple_solutions(self):
-        solve_model(create_model())
+        ampl = AMPL()
+        ampl.eval(
+            """var x binary; var y binary; var z binary;
+        minimize TotalSum: z + 1;
+        subj to C1 : x + y >= 1;"""
+        )
+        model = ampl.to_ampls(SOLVER)
+        model.set_option("sol:stub", "stub")
+        try:
+            model.set_option("sol:poolgap", 0.1)
+        except:
+            pass
+        model.refresh()
+        model.optimize()
+        ampl.import_solution(model)
+        v = int(ampl.get_value("TotalSum.nsol"))
+        if SOLVER == "xpress":
+            self.assertTrue(2 <= v <= 3)
+        else:
+            self.assertEqual(v, 3)
+        print(f"Got {v} solutions")
+        for i in range(1, v + 1):
+            ampl.import_solution("stub", i)
+            self.assertLessEqual(ampl.get_value("TotalSum"), 2.0)
+            ampl.display("TotalSum, x, y, z;")
 
 
 if __name__ == "__main__":
