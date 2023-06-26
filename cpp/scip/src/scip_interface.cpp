b0b485ff2d01cd29a7217ccfc446f55910da86b9
Jurgen Lentz <jurgen.lentz@rwth-aachen.de>
2023-06-26 15:35:50 +0200

    add error function

--- cpp/scip/src/scip_interface.cpp
+++ cpp/scip/src/scip_interface.cpp
@@ -4,25 +4,85 @@
 #include "scip_interface.h"
 #include "ampls/ampls.h"
 
+/**
+ * @brief translates a SCIP_RETCODE into an error string
+ *
+ * @param[in] retcode SCIP_RETCODE you want to translate
+ * @param[in] buffersize size of buffer
+ * @param[out] buffer_str buffer to character array to store translated message, this must be at least of size SCIP_MSG_MAX
+ * @return buffer_str or NULL, if retcode could not be translated
+ */
+inline char* SCIPgetErrorString(SCIP_RETCODE retcode, char* buffer_str, int buffersize)
+{
+   // the following was copied from SCIPprintError
+   switch(retcode)
+   {
+   case SCIP_OKAY:
+      (void) SCIPsnprintf(buffer_str, buffersize, "normal termination");
+      return buffer_str;
+   case SCIP_ERROR:
+      (void) SCIPsnprintf(buffer_str, buffersize, "unspecified error");
+      return buffer_str;
+   case SCIP_NOMEMORY:
+      (void) SCIPsnprintf(buffer_str, buffersize, "insufficient memory error");
+      return buffer_str;
+   case SCIP_READERROR:
+      (void) SCIPsnprintf(buffer_str, buffersize, "file read error");
+      return buffer_str;
+   case SCIP_WRITEERROR:
+      (void) SCIPsnprintf(buffer_str, buffersize, "file write error");
+      return buffer_str;
+   case SCIP_BRANCHERROR:
+      (void) SCIPsnprintf(buffer_str, buffersize, "branch error");
+      return buffer_str;
+   case SCIP_NOFILE:
+      (void) SCIPsnprintf(buffer_str, buffersize, "file not found error");
+      return buffer_str;
+   case SCIP_FILECREATEERROR:
+      (void) SCIPsnprintf(buffer_str, buffersize, "cannot create file");
+      return buffer_str;
+   case SCIP_LPERROR:
+      (void) SCIPsnprintf(buffer_str, buffersize, "error in LP solver");
+      return buffer_str;
+   case SCIP_NOPROBLEM:
+      (void) SCIPsnprintf(buffer_str, buffersize, "no problem exists");
+      return buffer_str;
+   case SCIP_INVALIDCALL:
+      (void) SCIPsnprintf(buffer_str, buffersize, "method cannot be called at this time in solution process");
+      return buffer_str;
+   case SCIP_INVALIDDATA:
+      (void) SCIPsnprintf(buffer_str, buffersize, "method cannot be called with this type of data");
+      return buffer_str;
+   case SCIP_INVALIDRESULT:
+      (void) SCIPsnprintf(buffer_str, buffersize, "method returned an invalid result code");
+      return buffer_str;
+   case SCIP_PLUGINNOTFOUND:
+      (void) SCIPsnprintf(buffer_str, buffersize, "a required plugin was not found");
+      return buffer_str;
+   case SCIP_PARAMETERUNKNOWN:
+      (void) SCIPsnprintf(buffer_str, buffersize, "the parameter with the given name was not found");
+      return buffer_str;
+   case SCIP_PARAMETERWRONGTYPE:
+      (void) SCIPsnprintf(buffer_str, buffersize, "the parameter is not of the expected type");
+      return buffer_str;
+   case SCIP_PARAMETERWRONGVAL:
+      (void) SCIPsnprintf(buffer_str, buffersize, "the value is invalid for the given parameter");
+      return buffer_str;
+   case SCIP_KEYALREADYEXISTING:
+      (void) SCIPsnprintf(buffer_str, buffersize, "the given key is already existing in table");
+      return buffer_str;
+   case SCIP_MAXDEPTHLEVEL:
+      (void) SCIPsnprintf(buffer_str, buffersize, "maximal branching depth level exceeded");
+      return buffer_str;
+   case SCIP_NOTIMPLEMENTED:
+      (void) SCIPsnprintf(buffer_str, buffersize, "function not implemented");
+      return buffer_str;
+   }
+   return NULL;
+}
+
 namespace ampls
 {
- 
-
-
-void impl::scip::cut_callback_wrapper(void* model, void* osicuts, void* appdata, int level, int pass)
-{
-  SCIPCallback* cb = (SCIPCallback*)appdata;
-  cb->where_ = ampls::Where::MIPSOL;
-  cb->model_ = model;
-  cb->osicuts_ = osicuts;
-  cb->run();
-}
-
-void impl::scip::callback_wrapper(SCIP* model, int msgno, int ndouble, const double* dvec, int nint, const int* ivec,
-  int nchar, char** cvec) {
-  //cb->where_ = ampls::Where::MSG;
-}
-
 
 SCIPDrv::~SCIPDrv() {
 }
@@ -79,13 +139,19 @@ void SCIPModel::optimize() {
 SCIPModel::~SCIPModel() {
   if (copied_)
     return;
-  impl::scip::AMPLSClose_scip(model_);
+  impl::scip::AMPLSClose_scip(solver_);
 }
 
 std::string SCIPModel::error(int code)
-{ //TODO
-  return "";
-  //return std::string(GRBgeterrormsg(getGRBenv()));
+{ 
+  char buffer[100];
+
+  if (SCIPgetErrorString((SCIP_RETCODE)code, buffer, 100) != NULL) {
+    return std::string(buffer);
+  }
+  else {
+    return "Error code not found.";
+  }
 }
 
 
