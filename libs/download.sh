37d420fa77689e96fedbb54804a48e1da646a963
Filipe Brandão <fdabrandao@gmail.com>
2023-06-30 11:12:59 +0100

    download.sh: Accept two directory structures for solver-public-libs.zip

--- libs/download.sh
+++ libs/download.sh
@@ -2,5 +2,6 @@
 cd "`dirname "$0"`"
 set -ex
 curl -OLk https://ampl.com/dl/fdabrandao/solver-public-libs.zip
+rm  -rf solver-public-libs
 unzip solver-public-libs.zip
-cp -r solver-public-libs/solver-public-libs/* .
+cp -r solver-public-libs/solver-public-libs/* . || cp -r solver-public-libs/* .
