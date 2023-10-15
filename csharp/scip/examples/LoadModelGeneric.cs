9b5abb9475656a17d2045a09e325c5deb6f1915f
Christian Valente <christian.valente@outlook.com>
2023-10-15 09:30:18 +0300

    Added SCIP csharp

---
+++ csharp/scip/examples/LoadModelGeneric.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ampls;
+
+namespace ampls_examples
+{
+  class LoadModelGeneric
+  {
+    public void run() { 
+
+    scipDrv g = new scipDrv();
+    var m = g.loadModel(@"D:\Development\AMPL\escrow-ampls\solvers_dist\test\models\tsp.nl");
+    DoStuff(m, "scip");
+    }
+
+    private void DoStuff(AMPLModel m, string name)
+    {
+      m.setAMPLParameter(SolverParams.SolverParameters.DBL_MIPGap, 0.2);
+      m.optimize();
+
+      // Print model solution result
+      string s = Enum.GetName(typeof(Status.SolStatus), m.getStatus());
+      double obj = m.getObj();
+      Console.WriteLine($"\n{s} solution with {name}={obj}");
+
+      // Get the solution vector and count the non-zeros
+      int nvars = m.getNumVars();
+      double[] solution = new double[nvars];
+      m.getSolution(0, nvars, solution);
+
+      int nnz = solution.Where(x => x != 0).Count();
+      Console.WriteLine($"Number of non zeros = {nvars}");
+
+      string solFileName = $"{m.getFileName()}-{name}.sol";
+      Console.WriteLine($"Writing solution file to {solFileName}");
+      m.writeSol(solFileName);
+    }
+
+      
+    
+    
+    }
+  }
+}
---
+++ csharp/scip/swig/scipsharp_c.i
@@ -0,0 +1,18 @@
+%module(directors = "1") gdsharp_c
+
+%include <arrays_csharp.i>
+CSHARP_ARRAYS(char*, string)
+CSHARP_ARRAYS(double, double)
+CSHARP_ARRAYS(int, int)
+CSHARP_ARRAYS(const double, double)
+CSHARP_ARRAYS(const int, int)
+
+%apply double OUTPUT[]{ double* sol }
+%apply double INPUT[]{ const double* coeffs }
+%apply int INPUT[]{ const int* vars }
+
+
+%include "../../common-csharp.i"
+%include "../../../cpp/ampls/swig/ampls-common.i"
+%include "../../../cpp/scip/swig/scip-common.i"
+
